8a7594d31faa96d3c4a3898786cfb5e7ab03a141
KarlaMartinez17 <karla.martinez_@hotmail.com>
2018-03-28 11:22:46 -0600

    se le agregaron 3 funciones en el JS para lograr la funcionalidad en el problema a resolver

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-console.log('cualquier cosa')
+var scores ={};
+console.log(scores);
+
+function createPlayer(object,name){
+  object[name.toLowerCase()] = 0;
+  console.log(object);
+}
+
+function addPoints(name,points){
+  scores[name.toLowerCase()] += points;
+  console.log(scores)
+}
+
+function printAllPoints(){
+  var arrayPlayers = Object.keys(scores);
+  var resultados = "";
+  for (var k = 0; k < arrayPlayers.length; k++) {
+
+if(k == arrayPlayers.length -1){ //se verifica en la ultimo index
+  resultados += arrayPlayers[k] + ':' + scores[arrayPlayers[k]];
+
+
+}else{
+  resultados += arrayPlayers[k] + ':'+ scores[arrayPlayers[k]] + ',';
+}
+  }//for
+  return resultados;
+}//function
+
+createPlayer(scores, "ana");
+addPoints("ana",20);
+addPoints("ana",40);
+createPlayer(scores,"silvana");
+addPoints("silvana",50);
+console.log(printAllPoints());
